bdf76749ffaaa2f844abac608598b53d6af839d7
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2023-10-19 02:44:40 -0500

    Users documentation was finished

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,33 +3,115 @@ import { createUser, getUser, authUser, deleteUser, updateUser } from '../contro
 
 const router = Router()
 
-// Users routing
-
 /**
  * @openapi
  * /api/user:
  *   post:
+ *     summary: Create User
  *     tags:
  *      - Users
- *     produces:
- *      - application/json
+ *     requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 example: Jhon
+ *               lastName:
+ *                 type: string
+ *                 example: Doe
+ *               email:
+ *                 type: string
+ *                 example: jhondoe@email.com
+ *               password:
+ *                 type: sring
+ *                 example: pass123
+ *      required: true
  *     responses:
  *       200:
- *         description: Holi.
+ *         description: Successful User creation
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: 651256e19c46e35b73b61588
+ *                  firstName:
+ *                    type: string
+ *                    example: Jhon
+ *                  lastName:
+ *                    type: string
+ *                    example: Doe
+ *                  email:
+ *                    type: string
+ *                    example: jhondoe@email.com
+ *                  password:
+ *                    type: sring
+ *                    example: pass123
+ *       400:
+ *         description: Failed User creation
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  error:
+ *                    type: string
+ *                    example: firstName is a wrong string
  */
 router.post('/', createUser)
 
 /**
  * @openapi
- * /api/user:
+ * /api/user/{userId}:
  *   get:
+ *     summary: Get User by id
  *     tags:
  *      - Users
  *     produces:
  *      - application/json
+ *     parameters:
+ *       - name: userId
+ *         in: json
+ *         required: true
+ *         type: string
  *     responses:
  *       200:
- *         description: Holi.
+ *         description: Successful delete User
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: 651256e19c46e35b73b61588
+ *                  firstName:
+ *                    type: string
+ *                    example: Jhon
+ *                  lastName:
+ *                    type: string
+ *                    example: Doe
+ *                  email:
+ *                    type: string
+ *                    example: jhondoe@email.com
+ *                  password:
+ *                    type: sring
+ *                    example: pass123
+ *       400:
+ *         description: Failed delete User
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  error:
+ *                    type: string
+ *                    example: Entered id not exists
  */
 router.get('/:userId', getUser)
 
@@ -37,41 +119,170 @@ router.get('/:userId', getUser)
  * @openapi
  * /api/user:
  *   put:
+ *     summary: Update User
  *     tags:
  *      - Users
- *     produces:
- *      - application/json
+ *     requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *                 example: 651256e19c46e35b73b61588
+ *               firstName:
+ *                 type: string
+ *                 example: Jhon
+ *               lastName:
+ *                 type: string
+ *                 example: Doe
+ *               email:
+ *                 type: string
+ *                 example: jhondoe@email.com
+ *               password:
+ *                 type: sring
+ *                 example: pass123
+ *      required: true
  *     responses:
  *       200:
- *         description: Holi.
+ *         description: Successful User update
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: 651256e19c46e35b73b61588
+ *                  firstName:
+ *                    type: string
+ *                    example: Jhon
+ *                  lastName:
+ *                    type: string
+ *                    example: Doe
+ *                  email:
+ *                    type: string
+ *                    example: jhondoe@email.com
+ *                  password:
+ *                    type: sring
+ *                    example: pass123
+ *       400:
+ *         description: Failed User update
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  error:
+ *                    type: string
+ *                    example: firstName is a wrong string
  */
 router.put('/', updateUser)
 
 /**
  * @openapi
- * /api/user:
+ * /api/user/{userId}:
  *   delete:
+ *     summary: Delete User
  *     tags:
  *      - Users
  *     produces:
  *      - application/json
+ *     parameters:
+ *       - name: userId
+ *         in: json
+ *         required: true
+ *         type: string
  *     responses:
  *       200:
- *         description: Holi.
+ *         description: Successful delete User
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: 651256e19c46e35b73b61588
+ *                  firstName:
+ *                    type: string
+ *                    example: Jhon
+ *                  lastName:
+ *                    type: string
+ *                    example: Doe
+ *                  email:
+ *                    type: string
+ *                    example: jhondoe@email.com
+ *                  password:
+ *                    type: sring
+ *                    example: pass123
+ *       400:
+ *         description: Failed delete User
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  error:
+ *                    type: string
+ *                    example: Entered id not exists
  */
 router.delete('/:userId', deleteUser)
 
 /**
  * @openapi
  * /api/user/auth:
- *   get:
+ *   post:
+ *     summary: Authenticate User
  *     tags:
  *      - Users
- *     produces:
- *      - application/json
+ *     requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: jhondoe@email.com
+ *               password:
+ *                 type: sring
+ *                 example: pass123
+ *      required: true
  *     responses:
  *       200:
- *         description: Holi.
+ *         description: Successful User authentication
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: 651256e19c46e35b73b61588
+ *                  firstName:
+ *                    type: string
+ *                    example: Jhon
+ *                  lastName:
+ *                    type: string
+ *                    example: Doe
+ *                  email:
+ *                    type: string
+ *                    example: jhondoe@email.com
+ *                  password:
+ *                    type: sring
+ *                    example: pass123
+ *       400:
+ *         description: Failed User authentication
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  error:
+ *                    type: string
+ *                    example: Credentials are incorrect
  */
 router.post('/auth', authUser)
 
